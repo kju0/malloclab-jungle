686afda7bc4c6bd2d1d8c8cf99d31a43de1aa5f7
kimjooyoung <jujulia3040@gmail.com>
2024-04-14 14:57:51 +0000

    [malloclab] explicit LIFO

--- mm.c
+++ mm.c
@@ -1,14 +1,3 @@
-/*
- * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
- * In this naive approach, a block is allocated by simply incrementing
- * the brk pointer.  A block is pure payload. There are no headers or
- * footers.  Blocks are never coalesced or reused. Realloc is
- * implemented directly using mm_malloc and mm_free.
- *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -23,15 +12,10 @@
  * provide your team information in the following struct.
  ********************************************************/
 team_t team = {
-    /* Team name */
-    "ateam",
-    /* First member's full name */
-    "Harry Bovik",
-    /* First member's email address */
-    "bovik@cs.cmu.edu",
-    /* Second member's full name (leave blank if none) */
+    "swjungle8-week5-7",
+    "KIM JOOYOUNG",
+    "jujulia3040@gmail.com",
     "",
-    /* Second member's email address (leave blank if none) */
     ""
 };
 
@@ -66,14 +50,20 @@ team_t team = {
 #define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
 #define PREV_BLKP(bp)   ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 
-static char *heap_listp;  // 처음에 쓸 큰 가용블록 힙을 만들어줌.
 
+#define GET_PRED(bp)    (*(void **)(bp))
+#define GET_SUCC(bp)    (*(void **)((char *)(bp) + WSIZE))
+
+static char *heap_listp;  // 처음에 쓸 큰 가용블록 힙을 만들어줌.
 
 //추가 코드
 static void *extend_heap(size_t words);
 static void *coalesce(void *bp);
 static void *find_fit(size_t asize);
 static void place(void *bp, size_t asize);
+static void set_no_free_block(void *bp);
+static void add_free_block(void *bp);
+
 
 /* 
  * mm_init - initialize the malloc package.
@@ -81,22 +71,33 @@ static void place(void *bp, size_t asize);
 int mm_init(void)
 {
     /* Create the initial empty heap */
-    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+    if ((heap_listp = mem_sbrk(8*WSIZE)) == (void *)-1)
         return -1;
     PUT(heap_listp, 0); /* Alignment padding */
 
     PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); /* Prologue header */
 
     PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1)); /* Prologue footer */
-    
-    PUT(heap_listp + (3*WSIZE), PACK(0, 1)); /* Epilogue header */
 
-    heap_listp += (2*WSIZE);
+    PUT(heap_listp + (3*WSIZE), PACK(4*WSIZE, 0)); //header
+
+    PUT(heap_listp + (4*WSIZE), NULL);
+
+    PUT(heap_listp + (5*WSIZE), NULL);
+
+    PUT(heap_listp + (6*WSIZE), PACK(4*WSIZE, 0)); //footer
+    
+    PUT(heap_listp + (7*WSIZE), PACK(0, 1)); /* Epilogue header */
+
+    heap_listp += (4*WSIZE); //첫 가용 블록의 pred 가리키기
+
     /* Extend the empty heap with a free block of CHUNKSIZE bytes */
+    // explicit-LIFO : headp init
     if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
         return -1;
     return 0;
 }
+
 
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
@@ -188,34 +189,44 @@ static void *coalesce(void *bp)
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
     if (prev_alloc && next_alloc) { /* Case 1 */
+        add_free_block(bp);
         return bp;
     }
     else if (prev_alloc && !next_alloc) { /* Case 2 */
+        set_no_free_block(NEXT_BLKP(bp));
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
+        
     }
     else if (!prev_alloc && next_alloc) { /* Case 3 */
+        set_no_free_block(PREV_BLKP(bp));
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
         PUT(FTRP(bp), PACK(size, 0));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
+
     }
     else { /* Case 4 */
+        set_no_free_block(NEXT_BLKP(bp));
+        set_no_free_block(PREV_BLKP(bp));
         size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
     }
+
+    // explicit-LIFO : 새로 들어간 bp 세팅
+    add_free_block(bp);
     return bp;
 }
 
 static void *find_fit(size_t asize)
 {
     void *bp;
-    for (bp=heap_listp; GET_SIZE(HDRP(bp))>0; bp=NEXT_BLKP(bp))
+    for (bp=heap_listp; bp!=NULL; bp=GET_SUCC(bp))
     {
-        if (!GET_ALLOC(HDRP(bp)) && GET_SIZE(HDRP(bp)) >= asize)
+        if (GET_SIZE(HDRP(bp)) >= asize)
             return bp;
     }
     return NULL;
@@ -226,15 +237,40 @@ static void place(void *bp, size_t asize)
     size_t csize = GET_SIZE(HDRP(bp));
     
     if ((csize - asize) >= (2*DSIZE)) {
+        set_no_free_block(bp); //가용 리스트에서 빼주기
         PUT(HDRP(bp), PACK(asize, 1));
         PUT(FTRP(bp), PACK(asize, 1));
+        
         bp = NEXT_BLKP(bp);
+        add_free_block(bp);
         PUT(HDRP(bp), PACK(csize-asize, 0));
         PUT(FTRP(bp), PACK(csize-asize, 0));
     }
     else {
+        set_no_free_block(bp);
         PUT(HDRP(bp), PACK(csize, 1));
         PUT(FTRP(bp), PACK(csize, 1));
     }
+}
 
+static void set_no_free_block(void *bp)
+{
+    //가용 리스트에서 빼주기
+    if (bp == heap_listp) {
+        heap_listp = GET_SUCC(bp);
+        return;
+    } else {
+        GET_SUCC(GET_PRED(bp)) = GET_SUCC(bp);
+    }
+    if (GET_SUCC(bp) != NULL) GET_PRED(GET_SUCC(bp)) = GET_PRED(bp);
+}
+
+static void add_free_block(void *bp)
+{
+    GET_SUCC(bp) = heap_listp;
+    //가용 리스트 요소가 한개일 경우
+    if (heap_listp != NULL){
+        GET_PRED(heap_listp) = bp;
+    }
+    heap_listp = bp;
 }
